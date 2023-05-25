69f23f44f90e1c23ea25973f1cdaa411d134e0c7
Игорь Пастушок <igorpastushok153@gmail.com>
2023-05-25 16:04:50 +0300

    commit 3

--- 10-http/index.js
+++ 10-http/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const EventEmitter = require('events')
-const PORT = 3000;
+const PORT = 3333;
 const emitter = new EventEmitter();
+const Router = require('../framework/Router')
+const Application = require('../framework/Application')
 // const server = http.createServer((req, res) => {
 //     // res.writeHead(200, {
 //     //     'Content-type': 'text/html; charset=utf-8'
@@ -21,64 +23,17 @@ const emitter = new EventEmitter();
 //
 // })
 
-class Router {
-    constructor() {
-        // endpoints = {
-        //     '/users': {
-        //         'GET': handler1,
-        //         'POST': handler2,
-        //         'DELETE': handler3,
-        //     }
-        // }
 
-        this.endpoints = {}
-    }
-
-    request(method = 'GET', path, handler) {
-        if (!this.endpoints[path]) {
-            this.endpoints[path] = {}
-        }
-        const endpoint = this.endpoints[path];
-
-        if (endpoint[method]) {
-            throw new Error(`[${method}] по адресу ${path} уже есть`)
-        }
-        endpoint[method] = handler
-        emitter.on(`[${path}]:[${method}]`, (req, res) => {
-            handler(req, res)
-        })
-    }
-
-    get(path, handler) {
-        this.request('GET', path,handler)
-    }
-    post(path, handler) {
-        this.request('POST', path,handler)
-    }
-    put(path, handler) {
-        this.request('PUT', path,handler)
-    }
-    delete(path, handler) {
-        this.request('DELETE', path,handler)
-    }
-}
-
+const app = new Application();
 const router = new Router();
 
 router.get('/users', (req, res) => {
     res.end('YOU SEND REQUEST TO /USERS')
 })
-
+console.log('router', router)
 router.get('/posts', (req, res) => {
     res.end('YOU SEND REQUEST TO /POSTS')
 })
+app.addRouter(router)
 
-const server = http.createServer((req, res) => {
-    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-    // return res.end('Home')
-    if (!emitted) {
-        res.end()
-    }
-})
-
-server.listen(PORT, () => console.log('server started on PORT = ', PORT))
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
